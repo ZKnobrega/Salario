6b0664bf5b9602ccfaccb876f8bf4b46f5d8abc0
ZKnobrega <54561781+ZKnobrega@users.noreply.github.com>
2020-03-17 14:50:57 -0300

    Update .java

--- .java
+++ .java
@@ -6,13 +6,13 @@ public class Hello {
   public static void main(String[] args)
   {
   Scanner valor = new Scanner (System.in);
-    double sal, salA;
+    Double sal, salA;
     System.out.print("Insira seu saláro Mensal: ");
     sal = valor.nextDouble();
     System.out.print("Insira valor do salario minimo: ");
     salA = valor.nextDouble();
-    System.out.print ("Você recebe : ");
-    System.out.print (sal/salA);
-    System.out.print (" Salários minimos");
+    System.out.printf ("Você recebe: %.2f", sal/salA);
+    System.out.print (" salarios minimos");
+
   }
 }
