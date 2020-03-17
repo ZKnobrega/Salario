19de417002319c2d384347f929ab230ff451f7be
ZKnobrega <54561781+ZKnobrega@users.noreply.github.com>
2020-03-17 01:06:07 -0300

    Update .java

--- .java
+++ .java
@@ -1,14 +1,18 @@
+package com.hello;
+
 import java.util.Scanner;
-public class Salario {
+public class Hello {
 
   public static void main(String[] args)
   {
   Scanner valor = new Scanner (System.in);
     double sal, salA;
+    System.out.print("Insira seu saláro Mensal: ");
     sal = valor.nextDouble();
+    System.out.print("Insira valor do salario minimo: ");
     salA = valor.nextDouble();
-    System.out.printf ("Você recebe : ");
+    System.out.print ("Você recebe : ");
     System.out.print (sal/salA);
-    System.out.print (" Salários");
+    System.out.print (" Salários minimos");
   }
 }
